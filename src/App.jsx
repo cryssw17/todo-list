dc7d0875af96dab0b9a0ffbd66af5d679e3bc67d
cryssw17 <cawash17@gmail.com>
2026-04-21 21:42:53 -0400

    implement-useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import './App.css'
 import TodoList from './TodoList.jsx';
 import TodoForm from './TodoForm.jsx';
+import {useState} from 'react';
+
+const todos = [
+    {id: 1, title: "clean room"},
+    {id: 2, title: "do laundry"},
+    {id: 3, title: "study"},
+    ]
 
 function App() {
+  const [todoList, setTodoList] = useState(todos);
   return (
     <div>
       <h1>Todo List</h1>
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,9 +1,4 @@
 function TodoList() {
-    const todoList = [
-    {id: 1, title: "clean room"},
-    {id: 2, title: "do laundry"},
-    {id: 3, title: "study"},
-    ]
     return (
         <ul>
         {todoList.map(todo => <li key={todo.id}>{todo.title}</li>)}
